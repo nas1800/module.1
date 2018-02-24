c6de4d38cddcae735189500a06c4ca816d7dc846
Trevoir Williams <trevoir.williams@gmail.com>
2018-02-24 11:59:47 -0500

    Added new form control.

--- sample.WindowsFormsApplication4/sample.WindowsFormsApplication4/Form2.Designer.cs
+++ sample.WindowsFormsApplication4/sample.WindowsFormsApplication4/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace sample.WindowsFormsApplication4
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -28,37 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // bindingSource1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.bindingSource1.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged);
             // 
-            // Form1
+            // Form2
             // 
-            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Maroon;
-            this.ClientSize = new System.Drawing.Size(291, 321);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(772, 402);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource bindingSource1;
     }
 }
-
--- sample.WindowsFormsApplication4/sample.WindowsFormsApplication4/Form2.cs
+++ sample.WindowsFormsApplication4/sample.WindowsFormsApplication4/Form2.cs
@@ -10,17 +10,16 @@ using System.Windows.Forms;
 
 namespace sample.WindowsFormsApplication4
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
         {
-            Form2 form = new Form2();
-            form.Show();
+
         }
     }
 }
